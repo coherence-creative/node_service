02a81c1247ed455ebb7ad5283cfcff33571e66ea
chaycode <chay.arnold@gmail.com>
2020-03-10 10:44:00 -0600

    w

--- email/emailService.js
+++ email/emailService.js
@@ -1,10 +1,52 @@
 const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 'chay.arnold@gmail.com',
-  from: 'chay.arnold@gmail.com',
-  subject: 'Sending with Twilio SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-sgMail.send(msg);
+const axios = require("axios")
+
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const redditData = async () => {
+    const response = await axios.get('https://api.reddit.com/r/funny/top/')
+    const data = await response
+    return data.data.data.children
+}
+
+// async function getReddit() {
+//     try {
+//         const response = await axios.get('https://api.reddit.com/r/funny/top/')
+//         const data = await response
+//         return data
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// async function getReddit() {
+//     let data;
+//     // fetch data from a url endpoint
+//     axios.get('https://api.reddit.com/r/funny/top/')
+//       .then((result) => {
+//         data = result
+//       });
+//     return data;
+//   }
+
+// shuttup = async () => {
+//     const res = await redditData()
+//     return res
+// }
+
+(async () => {
+    const response = await redditData();
+    console.log(response);
+  })();
+
+// console.log(shuttup())
+
+// const msg = {
+//   to: 'chay.arnold@gmail.com',
+//   from: 'chay.arnold@gmail.com',
+//   subject: 'Sending with Twilio SendGrid is Fun',
+//   text: 'and easy to do anywhere, even with Node.js',
+//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+// };
+
+// sgMail.send(msg);
